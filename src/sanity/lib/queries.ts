eeed5508f6b92dcecbc0926a7877aa77ae10156c
Jacob Friis Strand <hello@jacobfriisstrand.com>
2025-03-27 14:39:36 +0100

    feat: add generic image query

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -1,9 +1,20 @@
 import { defineQuery } from "next-sanity";
 
+export const IMAGE_QUERY = defineQuery(`{
+  ...,
+  asset-> {
+    _id,
+    _type,
+    metadata {
+      lqip
+    }
+  }
+}`);
+
 export const POSTS_QUERY = defineQuery(`*[_type == "post" && defined(slug.current)][0...12]{
   _id, title, slug
 }`);
 
 export const POST_QUERY = defineQuery(`*[_type == "post" && slug.current == $slug][0]{
-  title, body, mainImage
+  title, body, mainImage ${IMAGE_QUERY}
 }`);
