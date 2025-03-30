e44fd24f7a9c6d4a1f4a0c4b37b2708c92ab1190
Jacob Friis Strand <hello@jacobfriisstrand.com>
2025-03-30 14:33:38 +0200

    feat: add multiple schema types for structured content

--- src/sanity/schema-types/index.ts
+++ src/sanity/schema-types/index.ts
@@ -3,8 +3,23 @@ import type { SchemaTypeDefinition } from "sanity";
 import { authorType } from "./author-type";
 import { blockContentType } from "./block-content-type";
 import { categoryType } from "./category-type";
+import { faqsType } from "./components/faqs-type";
+import { featuresType } from "./components/features-type";
+import { heroType } from "./components/hero-type";
+import { textAndImageType } from "./components/text-and-image-type";
+import { pageType } from "./page-type";
 import { postType } from "./post-type";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, categoryType, postType, authorType],
+  types: [
+    blockContentType,
+    categoryType,
+    postType,
+    authorType,
+    pageType,
+    heroType,
+    textAndImageType,
+    featuresType,
+    faqsType,
+  ],
 };
