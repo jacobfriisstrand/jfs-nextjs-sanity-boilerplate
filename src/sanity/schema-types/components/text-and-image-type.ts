397f7200f48d40e9a2b9fd93138202d218290b32
Jacob Friis Strand <hello@jacobfriisstrand.com>
2025-03-30 14:33:18 +0200

    refactor: rename split image type to text and image type

--- src/sanity/schema-types/components/text-and-image-type.ts
+++ src/sanity/schema-types/components/text-and-image-type.ts
@@ -1,7 +1,7 @@
 import { defineField, defineType } from "sanity";
 
-export const splitImageType = defineType({
-  name: "splitImage",
+export const textAndImageType = defineType({
+  name: "textAndImage",
   type: "object",
   fields: [
     defineField({
