8ca9e9db62b8ce08bc2ddedfd16a93c4e132aadc
Jacob Friis Strand <hello@jacobfriisstrand.com>
2025-04-01 11:17:34 +0200

    feat: add possibility for custom image preview options for page builder components

--- src/sanity/schema-types/page-builder-type.ts
+++ src/sanity/schema-types/page-builder-type.ts
@@ -9,4 +9,17 @@ export const pageBuilderType = defineType({
     defineArrayMember({ type: "features" }),
     defineArrayMember({ type: "faqs" }),
   ],
+  // Possibilty for adding custom image previews (screenshots) of each component in the page builder.
+  // If desired, add a screenshot to the public folder and reference it here.
+  // The screenshot must be named exactly the same as the component type.
+  // options: {
+  //   insertMenu: {
+  //     views: [
+  //       {
+  //         name: "grid",
+  //         previewImageUrl: schemaType => `/block-previews/${schemaType}.png`,
+  //       },
+  //     ],
+  //   },
+  // },
 });
