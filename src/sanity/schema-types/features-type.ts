bd4f2d4ce9ac14543768bae7a3b609be7328ab00
Jacob Friis Strand <hello@jacobfriisstrand.com>
2025-05-01 13:31:50 +0200

    Initial commit

---
+++ src/sanity/schema-types/features-type.ts
@@ -0,0 +1,43 @@
+import { defineField, defineType } from "sanity";
+
+export const featuresType = defineType({
+  name: "features",
+  type: "object",
+  fields: [
+    defineField({
+      name: "title",
+      type: "string",
+    }),
+    defineField({
+      name: "features",
+      type: "array",
+      of: [
+        defineField({
+          name: "feature",
+          type: "object",
+          fields: [
+            defineField({
+              name: "title",
+              type: "string",
+            }),
+            defineField({
+              name: "text",
+              type: "string",
+            }),
+          ],
+        }),
+      ],
+    }),
+  ],
+  preview: {
+    select: {
+      title: "title",
+    },
+    prepare({ title }) {
+      return {
+        title,
+        subtitle: "Features",
+      };
+    },
+  },
+});
