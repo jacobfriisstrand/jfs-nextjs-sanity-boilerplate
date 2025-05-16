cf75ec8638786d8e526ba551ce4717634f034402
Jacob Friis Strand <hello@jacobfriisstrand.com>
2025-05-16 09:47:16 +0200

    feat: add footer component and update layout with grid styling

---
+++ src/components/modules/footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="bg-black px-3 py-12 text-center text-white">
+      <p className="font-mono text-[8vw] italic">GOAT Enterprises</p>
+      <p>&copy; 2039</p>
+    </footer>
+  );
+}
